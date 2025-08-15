2bf4071a630b133765b12ce2b30fe7a939010bae
derekd03 <134988441+derekd03@users.noreply.github.com>
2025-08-15 12:23:55 -0700

    Refactored opcodes

--- src/chip8.cpp
+++ src/chip8.cpp
@@ -1,6 +1,7 @@
 #include "chip8.h"
 #include <cstring>
 #include <cstdio>
+#include <cstdint>
 
 void Chip8::initialize()
 {
@@ -47,16 +48,15 @@ void Chip8::emulateCycle()
     // Decode Opcode
     switch (opcode & 0xF000)
     {
-    /* Some opcodes */
     case 0x0000:
         switch (opcode & 0x000F)
         {
-        case 0x0000: // 0x00E0: Clears the screen
+        case 0x0000: // 00E0: Clears the screen
             memset(gfx, 0, sizeof(gfx));
             pc += 2;
             break;
 
-        case 0x000E: // 0x00EE: Returns from subroutine
+        case 0x000E: // 00EE: Returns from subroutine
             pc = stack[--sp];
             break;
 
@@ -65,43 +65,41 @@ void Chip8::emulateCycle()
         }
         break;
 
-    case 0x0004: // 0x8YX: Adds VY to VX, set VF if carry
-        if (V[(opcode & 0x00F0) >> 4] > (0xFF - V[(opcode & 0x0F00) >> 8]))
-        {
-            V[0xF] = 1; // Set carry flag
-        }
-        else
-        {
-            V[0xF] = 0; // Clear carry flag
-        }
-        // Add VY to VX
-        V[(opcode & 0x00F0) >> 4] += V[(opcode & 0x0F00) >> 8];
-        pc += 2;
-        break;
-
-    case 0x0033: // 0xFX33: Stores BCD representation of VX in memory locations I, I+1, and I+2
-        memory[I] = V[(opcode & 0x0F00) >> 8] / 100;
-        memory[I + 1] = (V[(opcode & 0x0F00) >> 8] / 10) % 10;
-        memory[I + 2] = V[(opcode & 0x0F00) >> 8] % 10;
-        pc += 2;
-        break;
-
     case 0x2000: // 2NNN: Calls subroutine at NNN
         stack[sp] = pc;
         ++sp;
         pc = opcode & 0x0FFF;
         break;
 
+    case 0x8000:
+        switch (opcode & 0x000F)
+        {
+        case 0x0004: // 8XY4: Adds VY to VX, set VF if carry
+        {
+            uint8_t x = (opcode & 0x0F00) >> 8;
+            uint8_t y = (opcode & 0x00F0) >> 4;
+            if (V[y] > (0xFF - V[x]))
+                V[0xF] = 1; // Set carry flag
+            else
+                V[0xF] = 0; // Clear carry flag
+            V[x] += V[y];
+            pc += 2;
+            break;
+        }
+        default:
+            printf("Unknown 0x8XY_ opcode: 0x%X\n", opcode);
+            break;
+        }
+        break;
+
     case 0xA000: // ANNN: Sets I to the address NNN
-        // Execute opcode
         I = opcode & 0x0FFF;
         pc += 2;
         break;
 
-    case 0xD000:
+    case 0xD000: // DXYN: Draws a sprite at (VX, VY) with N bytes of data starting at memory location I
     {
-        // Fetch position and height of the sprite
-        unsigned short x = V[(opcode & 0x0F00) >> 4];
+        unsigned short x = V[(opcode & 0x0F00) >> 8];
         unsigned short y = V[(opcode & 0x00F0) >> 4];
         unsigned short height = opcode & 0x000F;
         unsigned short pixel;
@@ -115,59 +113,55 @@ void Chip8::emulateCycle()
             {
                 if ((pixel & (0x80 >> xline)) != 0)
                 {
-                    // Check if the current evaluated pixel is set to 1
                     if (gfx[(x + xline + ((y + yline) * 64))] == 1)
-                        V[0xF] = 1; // Set collision flag
-                    // Set pixel value using XOR
+                        V[0xF] = 1;                           // Set collision flag
                     gfx[x + xline + ((y + yline) * 64)] ^= 1; // Toggle pixel
                 }
             }
         }
-        // Update the screen
         drawFlag = true;
         pc += 2;
+        break;
     }
 
     case 0xE000:
         switch (opcode & 0x00FF)
         {
-        // EX9E: Skips the next instruction
-        // if the key stored in VX is pressed
-        case 0x009E:
+        case 0x009E: // EX9E: Skips the next instruction if the key stored in VX is pressed
             if (key[V[(opcode & 0x0F00) >> 8]] != 0)
                 pc += 4; // Skip next instruction
             else
                 pc += 2; // Go to next instruction
             break;
+        default:
+            printf("Unknown 0xEX__ opcode: 0x%X\n", opcode);
+            break;
         }
+        break;
 
-    /* More opcodes */
+    case 0xF000:
+        switch (opcode & 0x00FF)
+        {
+        case 0x0033: // FX33: Stores BCD representation of VX in memory locations I, I+1, and I+2
+        {
+            uint8_t x = (opcode & 0x0F00) >> 8;
+            memory[I] = V[x] / 100;
+            memory[I + 1] = (V[x] / 10) % 10;
+            memory[I + 2] = V[x] % 10;
+            pc += 2;
+            break;
+        }
+        default:
+            printf("Unknown 0xFX__ opcode: 0x%X\n", opcode);
+            break;
+        }
+        break;
+
+        /* TODO: More opcodes */
+
     default:
         printf("Unknown opcode: 0x%X\n", opcode);
     }
-    /*
-    Check the opcode table to see what the opcode means
-    0xAF0 // Assembly: mvi 2F0h
-
-    If we take a look at the opcode table, it tells us the following:
-
-    ANNN: Sets I to the address NNN
-
-    We will need to set the index register I to the value of NNN (0x2F0)
-    */
-
-    // Execute Opcode
-    /*
-    1010001011110000 & // 0xA2F0 (opcode)
-    0000111111111111 = // 0x0FFF
-    ------------------
-    0000001011110000   // 0x02F0 (0x2F0)
-
-    Resulting code:
-
-    I = opcode & 0x0FFF;
-    pc += 2; // increment program counter by 2 after every executed opcode unless you call a subroutine
-    */
 
     // Update timers
     if (delay_timer > 0)
